bfeec0dbf6a6e1255b5bb3dadc93948df2126e47
JyJy32 <joren.liegeois@gmail.com>
2024-01-02 14:27:44 +0100

    windows support sort of

--- logger.go
+++ logger.go
@@ -2,15 +2,32 @@ package logger
 
 import (
 	"fmt"
-	"time"
+    "runtime"
+    "time"
 )
 
+var http string = ""
+var mosquitto string = ""
+var debug string = ""
+var warning string = ""
+var error string = ""
 //prefix for logger level
-var http string = "[\033[32;1mHTTP\033[0m]"
-var mosquitto string = "[\033[34;1mMQTT\033[0m]"
-var debug string = "[\033[36mDEBG\033[0m]"
-var warning string = "[\033[33mWARN\033[0m]"
-var error string = "[\033[31mERR\033[0m ]"
+func Init() {
+    if runtime.GOOS == "windows" {
+        http = "[HTTP]"
+        mosquitto = "[MQTT]"
+        debug = "[DEBG]"
+        warning = "[WARN]"
+        error = "[ERR ]"
+        Debug("no colours on windows")
+    } else {
+        http = "[\033[32;1mHTTP\033[0m]"
+        mosquitto = "[\033[34;1mMQTT\033[0m]"
+        debug = "[\033[36mDEBG\033[0m]"
+        warning = "[\033[33mWARN\033[0m]"
+        error = "[\033[31mERR\033[0m ]"
+    }
+}
 
 func Http(msg ...any) {
 	hour, min, sec := time.Now().Clock()
